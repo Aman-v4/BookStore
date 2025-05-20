d868f2f271f85f86299426d827722da7d992d201
Aman <namaverma435@gmail.com>
2025-05-20 11:47:50 +0530

    dummy

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (<footer className="bg-gray-100">
@@ -41,19 +42,24 @@ const Footer = () => {
               className="mt-12 flex flex-wrap justify-center gap-6 md:gap-8 lg:mt-0 lg:justify-end lg:gap-12"
             >
               <li>
-                <a className="text-gray-700 transition hover:text-gray-700/75" href="#"> Explore </a>
+                <Link
+                to="/Explore"
+                className="text-gray-700 transition hover:text-gray-700/75" > Explore </Link>
               </li>
-      
               <li>
-                <a className="text-gray-700 transition hover:text-gray-700/75" href="#"> Wishlist </a>
+                <Link
+                to="/Wishlist"
+                className="text-gray-700 transition hover:text-gray-700/75" > Wishlist </Link>
               </li>
-      
               <li>
-                <a className="text-gray-700 transition hover:text-gray-700/75" href="#"> Cart </a>
+                <Link
+                to="/Cart"
+                className="text-gray-700 transition hover:text-gray-700/75" > Cart </Link>
               </li>
-      
               <li>
-                <a className="text-gray-700 transition hover:text-gray-700/75" href="#"> Orders </a>
+                <Link
+                to="/Orders"
+                className="text-gray-700 transition hover:text-gray-700/75" > Orders </Link>
               </li>
             </ul>
           </div>
